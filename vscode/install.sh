1e8ebf07d0f96a6a7f886b8e4156a7b4ab1aec54
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-08-17 20:44:21 +0200

    add github ext to vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,3 +1,4 @@
 code --install-extension EditorConfig.EditorConfig
 code --install-extension mikestead.dotenv
 code --install-extension vscodevim.vim
+code --install-extension GitHub.vscode-pull-request-github 
