f8f3909b51faa2f513b83c6fab1f64fac54d7bea
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-08-18 18:34:22 +0200

    Add touchbar utils for VSCode

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,3 +2,4 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension mikestead.dotenv
 code --install-extension vscodevim.vim
 code --install-extension GitHub.vscode-pull-request-github 
+code --install-extension felipe.nasc-touchbar
