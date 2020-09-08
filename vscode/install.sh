4de082ab8a1efc14067b1d3bacc446c0e222dd55
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-09-08 16:23:03 +0200

    Add Elixir VSCode ext

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,6 @@
 code --install-extension EditorConfig.EditorConfig
 code --install-extension mikestead.dotenv
+code --install-extension mjmcloug.vscode-elixir
 code --install-extension vscodevim.vim
 code --install-extension GitHub.vscode-pull-request-github 
 code --install-extension felipe.nasc-touchbar
