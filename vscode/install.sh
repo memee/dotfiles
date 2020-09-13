597ee6a745951f34639fcaf50f11a0fa4e46b11c
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-09-13 21:10:54 +0200

    Add elm vscode extension|

--- vscode/install.sh
+++ vscode/install.sh
@@ -5,3 +5,4 @@ code --install-extension vscodevim.vim
 code --install-extension GitHub.vscode-pull-request-github 
 code --install-extension felipe.nasc-touchbar
 code --install-extension esbenp.prettier-vscode
+code --install-extension elmtooling.elm-ls-vscode
