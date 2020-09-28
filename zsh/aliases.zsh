d115cb4344d0f332f09557d4735adade6b704f4b
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-09-28 13:30:43 +0200

    Install awscli

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,6 @@
 # restarts core audio to fix crackling
 alias killaudio="sudo kill -9 $(ps ax|grep 'coreaudio[a-z]' | awk '{print $1}')"
+
+# Copy public key to clipboard:
+alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
